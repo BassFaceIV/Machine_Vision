e75b1e31b95514641e2a39e1fd21995b665c9901
Beutel <cabiv20@gmail.com>
2015-08-06 22:33:52 -0400

    minor changes to image unpacking functionality

--- unpack_image.py
+++ unpack_image.py
@@ -1,1 +1,10 @@
 from PIL import Image
+
+inputImage = sys.argv[1]
+image = Image.open(inputImage, 'r')
+pixels = list(image.getData())
+
+pixelData = open("pixelData", 'w')
+for pixel in pixels:
+	pixelData.write(pixel)
+pixelData.close()
